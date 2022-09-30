95a3209e5556a0dd37603787cc4058806d6cae29
Truth-hash <86872654+Truth-hash@users.noreply.github.com>
2022-09-30 18:38:00 +0000

    all details complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
       fontSize: {
         xxs: "0.625rem",
       },
+      colors: {
+        Gray_0: "#4E5150",
+        Gray_1: "#333333",
+        Gray_2: "#4F4F4F",
+        Gray_3: "#828282",
+        Gray_4: "#BDBDBD",
+        Gray_5: "#E0E0E0",
+        Gray_6: "#F2F2F2",
+        Orange: "#F2994A",
+      },
     },
   },
   plugins: [],
