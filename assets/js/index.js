c07a593549850bdddff6705c5a0fb8b0351aa831
Zerebel <kofitruth11@gmail.com>
2022-11-06 01:06:28 +0000

    form select updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,7 @@ const subVintage = document.querySelector('#vintage_minus');
 const addLevi = document.querySelector('#levi_add');
 const subLevi = document.querySelector('#levi_sub');
 const shipping = document.querySelector('#shipping');
+const countrySelect = document.querySelector('#country');
 
 const num = {
   levi: 1,
@@ -39,7 +40,9 @@ const num = {
   },
   total: () => {
     const newTotal = (54.99 * num.vintage + 74.99 * num.levi).toFixed(2);
-    shipping.textContent = `$${num.shipping * 2}`;
+    shipping.textContent = `$${
+      num.levi == 0 && num.vintage == 0 ? '0' : num.shipping
+    }`;
     return (total.textContent = `$${newTotal}`);
   },
 };
@@ -48,3 +51,32 @@ addLevi.addEventListener('click', num.leviAdd, false);
 subLevi.addEventListener('click', num.leviMinus, false);
 addVintage.addEventListener('click', num.vintageAdd, false);
 subVintage.addEventListener('click', num.vintageMinus, false);
+
+// country elements
+const getData = async () => {
+  const url = `https://restcountries.com/v3.1/all`;
+  const data = await fetch(url);
+  const country = await data.json();
+  return country;
+};
+
+if (localStorage.getItem('country') == undefined) {
+  getData().then((country) => {
+    const countryNames = [];
+    for (const items of country) {
+      countryNames.push(items?.name?.common);
+    }
+    return localStorage.setItem('country', JSON.stringify(countryNames));
+  });
+}
+
+const countryElements = () => {
+  const country = JSON.parse(localStorage.getItem('country'));
+  country.sort();
+  for (const item of country) {
+    const countryName = document.createElement('option');
+    countryName.textContent = item;
+    countrySelect.appendChild(countryName);
+  }
+};
+countryElements();
